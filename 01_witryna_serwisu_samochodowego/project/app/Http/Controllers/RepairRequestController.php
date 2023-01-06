4e348425ca43a8ef98d3328a462661e2dd64c0c4
Marcin Słyś <marcin.slys1541@gmail.com>
2023-01-06 19:05:11 +0100

    RepairRequestController - request.index for all type of users

--- 01_witryna_serwisu_samochodowego/project/app/Http/Controllers/RepairRequestController.php
+++ 01_witryna_serwisu_samochodowego/project/app/Http/Controllers/RepairRequestController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\RepairRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+
+use App\Helpers\HasEnsure;
+use App\Models\Book;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use Illuminate\View\View;
 
 class RepairRequestController extends Controller
 {
-    //
+    use HasEnsure;
+
+    public function index(): View
+    {
+        $books = RepairRequest::all();
+        $userID = Auth::id();
+        $userType = User::where('ID', $userID)->value('type');
+        switch($userType) {
+            case 1:
+                $requests = RepairRequest::where('clientID', $userID)->orderBy('id', 'desc')->get();
+                return view('requests.index_user')->with('requests', $requests);
+            case 2:
+                $requests = RepairRequest::where('status', 0)->orderBy('id', 'desc')->get();
+                return view('requests.index_employee')->with('requests', $requests);
+            case 3:
+                $requests = RepairRequest::orderBy('id', 'desc')->get();
+                return view('requests.index_admin')->with('requests', $requests);
+        }
+        return view('welcome');
+    }
 }
---
+++ 01_witryna_serwisu_samochodowego/project/resources/views/requests/index_employee.blade.php
@@ -0,0 +1,50 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('List of all waiting requests') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
+                @if($requests->isEmpty())
+                    <p class="p-6">No requests in database.</p>
+                @else
+                    <table class="min-w-full divide-y divide-gray-200 mt-4">
+                        <thead class="bg-gray-50">
+                        <tr>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Title of service
+                            </th>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Client
+                            </th>
+                            <th scope="col" class="relative px-6 py-3">
+                                <span class="sr-only">Details</span>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                        @foreach($requests as $request)
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-500">{{ $request->title }}</div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-900">{{ App\Models\User::where('id', $request->clientID )->value('name') }}</div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                    <a href="{{ route('requests.show', $request) }}" class="text-indigo-600 hover:text-indigo-900">Details</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                @endif
+            </div>
+        </div>
+    </div>
+</x-app-layout>
