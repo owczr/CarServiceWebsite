ea881c2ec86f040755a55d2a91b4ddda62659324
Jakub Owczarek <jowczarek@student.agh.edu.pl>
2023-01-08 23:08:39 +0100

    Change user request form

--- 01_witryna_serwisu_samochodowego/project/resources/views/requests/create.blade.php
+++ 01_witryna_serwisu_samochodowego/project/resources/views/requests/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Creating a book') }}
+            {{ __('Creating a Repair Request') }}
         </h2>
     </x-slot>
     <div class="py-12">
@@ -9,22 +9,22 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
-                    <form method="post" action="{{ route('books.store') }}">
+                    <form method="post" action="{{ route('requests.create') }}">
 
                         @csrf
 
                         <div>
-                            <x-input-label for="isbn" :value="__('ISBN')" />
-                            <x-text-input id="isbn" class="block mt-1 w-full" type="text" name="isbn" :value="old('isbn')" autofocus />
+                            <x-input-label for="title" :value="__('Title')" />
+                            <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')" autofocus />
 
-                            <x-input-error :messages="$errors->get('isbn')" class="mt-2" />
+                            <x-input-error :messages="$errors->get('title')" class="mt-2" />
                         </div>
 
                         <div class="mt-4">
-                            <x-input-label for="title" :value="__('Title')" />
-                            <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')" />
+                            <x-input-label for="model" :value="__('Model')" />
+                            <x-text-input id="model" class="block mt-1 w-full" type="text" name="model" :value="old('model')" />
 
-                            <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                            <x-input-error :messages="$errors->get('model')" class="mt-2" />
                         </div>
 
                         <div class="mt-4">
@@ -32,6 +32,20 @@
                             <x-text-input id="description" class="block mt-1 w-full" type="text" name="description" :value="old('description')" />
 
                             <x-input-error :messages="$errors->get('description')" class="mt-2" />
+                        </div>
+
+                        <div class="mt-4">
+                            <x-input-label for="image" :value="__('Image')" />
+                        </div>
+
+                        <div class="mt-2">
+                        <form action="{{ route('requests.create') }}" method="post" enctype="multipart/form-data">
+                                <!-- Add CSRF Token -->
+                                @csrf
+                                <div class="form-group">
+                                    <input type="file" name="file" required>
+                                </div>
+                            </form>
                         </div>
 
                         <div class="flex items-center justify-end mt-4">
