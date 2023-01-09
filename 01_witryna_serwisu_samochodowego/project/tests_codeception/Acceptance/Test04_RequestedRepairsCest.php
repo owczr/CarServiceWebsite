6cc78169b11b735277870f8d851bec1a6d8d4df7
Jakub Owczarek <jowczarek@student.agh.edu.pl>
2023-01-09 23:15:59 +0100

    Add acceptance test for testing whether user can see his requests

---
+++ 01_witryna_serwisu_samochodowego/project/tests_codeception/Acceptance/Test04_RequestedRepairsCest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace TestsCodeception\Acceptance;
+
+use Illuminate\Support\Facades\Auth;
+use TestsCodeception\Support\AcceptanceTester;
+
+class Test04_RequestedRepairsCest
+{
+    public function requestedRepairsTest(AcceptanceTester $I): void
+    {
+        $I->wantTo('see my requested repairs');
+
+        $I->amOnPage('/login');
+        $I->seeCurrentUrlEquals('/login');
+        $I->fillField('email', 'john.doe@gmail.com');
+        $I->fillField('password', 'secret');
+        $I->click('Log in');
+        $I->seeCurrentUrlEquals('/dashboard');
+
+        $clientID = 2;
+        $title = 'moje ohv chce miodu';
+        $model = 'fso polonez';
+        $description = 'wygodna kanapki mientkie nie trzensie hoho sunie jak diabel po szosie';
+        $status = 0;
+
+        $id = $I->haveInDatabase('repair_requests', ['clientID' => $clientID, 'title' => $title,
+            'model' => $model, 'description' => $description, 'status' => $status]);
+
+        $I->amOnPage('/requests');
+        $I->seeCurrentUrlEquals('/requests');
+
+        $I->see("Details");
+        $I->click("Details");
+
+        $I->seeCurrentUrlEquals('/requests/'.$id);
+
+        $I->seeInDatabase('repair_requests', ['title' => $title,
+            'model' => $model, 'description' => $description]);
+
+        $I->see($title);
+        $I->see($model);
+        $I->see($description);
+    }
+}
