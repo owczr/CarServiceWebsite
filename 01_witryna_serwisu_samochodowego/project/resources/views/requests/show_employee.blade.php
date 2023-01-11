9b70ca156adeffb63956106fa82ab227aceaad52
Marcin Słyś <marcin.slys1541@gmail.com>
2023-01-11 18:59:58 +0100

    employee - show order - test

--- 01_witryna_serwisu_samochodowego/project/resources/views/requests/show_employee.blade.php
+++ 01_witryna_serwisu_samochodowego/project/resources/views/requests/show_employee.blade.php
@@ -112,7 +112,7 @@
                                     <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                                         @foreach(explode('|',$request->images) as $image)
                                             @if($image != "")
-                                                <img src="{{ asset( "$image")  }}" style="width: 400px; margin-left: auto; margin-right: auto; display: block;" alt="{{ explode('/',$image)[1] }}"/>
+                                                <img src="{{ asset( "$image")  }}" style="width: 400px; margin-left: auto; margin-right: auto; display: block;" alt="{{ explode('/',$image)[1] }}"sl/>
                                             @endif
                                         @endforeach
                                     </dd>
