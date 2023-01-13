91d5b8536ef79e2cd51e1536740c0370e9dd92f0
Jakub Owczarek <jakub.owczarke@gmail.com>
2023-01-13 17:50:31 +0100

    Add simple employee details

--- 01_witryna_serwisu_samochodowego/project/resources/views/employees/show.blade.php
+++ 01_witryna_serwisu_samochodowego/project/resources/views/employees/show.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            hello
         </h2>
     </x-slot>
     <div class="py-12">
@@ -10,7 +11,7 @@
                 <div class="bg-white shadow overflow-hidden sm:rounded-lg">
                     <div class="px-4 py-5 sm:px-6 mt-4 mb-4">
                         <h3 class="text-lg leading-6 font-medium text-gray-900">
-                            hello
+                            Employee no #{{ $user->id }}: {{ $user->name }}
                         </h3>
                         <p class="mt-1 max-w-2xl text-sm text-gray-500">
                             Detailed information.
@@ -20,54 +21,28 @@
                         <dl>
                             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt class="text-sm font-medium text-gray-500">
-                                    Client name
+                                    Name
                                 </dt>
                                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {{ App\Models\User::where('id', $user->id )->value('name') }}
                                 </dd>
                             </div>
                             <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt class="text-sm font-medium text-gray-500">
-                                    Client phone number
+                                    Phone number
                                 </dt>
                                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {{ App\Models\User::where('id', $user->id )->value('phone') }}
                                 </dd>
                             </div>
                             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt class="text-sm font-medium text-gray-500">
-                                    Title
+                                    Email
                                 </dt>
                                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    {{ $user->email }}
                                 </dd>
                             </div>
-                            <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt class="text-sm font-medium text-gray-500">
-                                    Car model
-                                </dt>
-                                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                </dd>
-                            </div>
-                            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt class="text-sm font-medium text-gray-500">
-                                    Proposed date
-                                </dt>
-                                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                </dd>
-                            </div>
-                            <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt class="text-sm font-medium text-gray-500">
-                                    Status
-                                </dt>
-
-
-                            </div>
-                            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt class="text-sm font-medium text-gray-500">
-                                    Description
-                                </dt>
-                                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                </dd>
-                            </div>
-
 
                         </dl>
                     </div>
@@ -76,3 +51,4 @@
         </div>
     </div>
 </x-app-layout>
+
