cb3c5c0b7e77e2c500ea5fa5da4f1b80b041ee70
Shamte Ndiaye <ndiaye@student.agh.edu.pl>
2023-01-13 18:17:48 +0100

    user's dashboard - test

---
+++ 01_witryna_serwisu_samochodowego/project/tests_codeception/Acceptance/Test20_User_DashboardCest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TestsCodeception\Acceptance;
+
+use TestsCodeception\Support\AcceptanceTester;
+
+class Test20_User_DashboardCest
+{
+    public function dashboardTest(AcceptanceTester $I): void
+    {
+        $I->wantTo("See my (user's) dashboard page");
+
+        $I->amOnPage('/login');
+        $I->seeCurrentUrlEquals('/login');
+        $I->fillField('email', 'client1@gmail.com');
+        $I->fillField('password', 'secret');
+        $I->click('Log in');
+        $I->seeCurrentUrlEquals('/dashboard');
+
+        $I->see("Welcome to Better than worst mechanics!");
+
+        $I->see("Requests & Orders");
+        $I->click("Requests & Orders");
+        $I->seeCurrentUrlEquals('/requests');
+        $I->see('List of all services');
+    }
+}
