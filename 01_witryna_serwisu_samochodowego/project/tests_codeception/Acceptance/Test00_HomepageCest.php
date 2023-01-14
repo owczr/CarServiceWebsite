fbbbd9a5e99eff8478dc67011ae38a3ef511b0b4
Patryk Sroka <psroka@student.agh.edu.pl>
2023-01-14 20:18:16 +0100

    homepage test + gallery begin

--- 01_witryna_serwisu_samochodowego/project/tests_codeception/Acceptance/Test00_HomepageCest.php
+++ 01_witryna_serwisu_samochodowego/project/tests_codeception/Acceptance/Test00_HomepageCest.php
@@ -8,12 +8,26 @@ class Test00_HomepageCest
 {
     public function homepageTest(AcceptanceTester $I): void
     {
-        $I->wantTo('Dashboard');
+        $I->wantTo('View homepage');
 
         $I->amOnPage('/');
 
         $I->seeInTitle('Better than Worst mechanics!');
 
-        $I->seeLink("Create a request", "route('requests')");
+        $I->see("Log in");
+        $I->click("Log in");
+        $I->seeCurrentUrlEquals('/login');
+
+        $I->amOnPage('/');
+
+        $I->see("Register");
+        $I->click("Register");
+        $I->seeCurrentUrlEquals('/register');
+
+        $I->amOnPage('/');
+
+        $I->see("Create a request");
+        $I->click("Create a request");
+        $I->seeCurrentUrlEquals('/login');
     }
 }
