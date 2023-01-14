86fdcff8cd0b2021abd7897a1b58fb4635cdf4e4
Patryk Sroka <psroka@student.agh.edu.pl>
2023-01-14 22:59:54 +0100

    Prices + test, optimalization of Views

---
+++ 01_witryna_serwisu_samochodowego/project/resources/views/prices.blade.php
@@ -0,0 +1,99 @@
+<x-app-layout>
+    <style>
+        body {
+            font-family: 'lato', sans-serif;
+        }
+        .container {
+            padding-top: 30px;
+            max-width: 1000px;
+            margin-left: auto;
+            margin-right: auto;
+            padding-left: 10px;
+            padding-right: 10px;
+        }
+
+        h2 {
+            font-size: 26px;
+            margin: 20px 0;
+            text-align: center;
+        }
+
+        li {
+            border-radius: 3px;
+            padding: 25px 30px;
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 25px;
+        }
+        .table-header {
+            background-color: #95A5A6;
+            font-size: 14px;
+            text-transform: uppercase;
+            letter-spacing: 0.03em;
+        }
+        .table-row {
+            background-color: #ffffff;
+            box-shadow: 0px 0px 9px 0px rgba(0,0,0,0.1);
+        }
+        .col-1 {
+            flex-basis: 10%;
+        }
+        .col-3 {
+            flex-basis: 25%;
+        }
+        .col-4 {
+            flex-basis: 25%;
+        }
+
+        @media all and (max-width: 767px) {
+            .table-header {
+                display: none;
+            }
+            .table-row{
+
+            }
+            li {
+                display: block;
+            }
+            .col {
+
+                flex-basis: 100%;
+
+            }
+            .col {
+                display: flex;
+                padding: 10px 0;
+        }
+        }
+    </style>
+
+    <div class="container">
+        <ul class="responsive-table">
+            <li class="table-header">
+                <div class="col col-1">Job</div>
+                <div class="col col-3">Amount Due</div>
+                <div class="col col-4">Approximate time</div>
+            </li>
+            <li class="table-row">
+                <div class="col col-1" data-label="Job">Breaks</div>
+                <div class="col col-3" data-label="Amount">$150</div>
+                <div class="col col-4" data-label="Payment Status">2 hours</div>
+            </li>
+            <li class="table-row">
+                <div class="col col-1" data-label="Job">Tires</div>
+                <div class="col col-3" data-label="Amount">$100</div>
+                <div class="col col-4" data-label="Payment Status">1 hour</div>
+            </li>
+            <li class="table-row">
+                <div class="col col-1" data-label="Job">Window tinting</div>
+                <div class="col col-3" data-label="Amount">$50</div>
+                <div class="col col-4" data-label="Payment Status">2 days</div>
+            </li>
+            <li class="table-row">
+                <div class="col col-1" data-label="Job">Aplying PPF</div>
+                <div class="col col-3" data-label="Amount">$1100</div>
+                <div class="col col-4" data-label="Payment Status">3 days</div>
+            </li>
+        </ul>
+    </div>
+</x-app-layout>
