354f8ae59038666c1ee5bfa945071b3e37686319
Jakub Owczarek <jakub.owczarke@gmail.com>
2023-01-15 13:37:30 +0100

    Fix typos

--- 01_witryna_serwisu_samochodowego/project/resources/views/employees/show.blade.php
+++ 01_witryna_serwisu_samochodowego/project/resources/views/employees/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            hello
+            Viewing employee details
         </h2>
     </x-slot>
     <div class="py-12">
