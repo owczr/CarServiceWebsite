bc194962d9dddd617c4d4efd59452a8bf127f375
Jakub Owczarek <jakub.owczarke@gmail.com>
2023-01-15 13:37:30 +0100

    Fix typos

--- 01_witryna_serwisu_samochodowego/project/resources/views/welcome.blade.php
+++ 01_witryna_serwisu_samochodowego/project/resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    create a request to your mechanics.
+                                    Create a request to your mechanics.
                                 </div>
                             </div>
                         </div>
@@ -76,7 +76,7 @@
                                     <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
                                     <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                                 </svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white" style="font-size: 1.5em">Stuff</div>
+                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white" style="font-size: 1.5em">Staff</div>
                             </div>
 
                             <div class="ml-12">
@@ -100,7 +100,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Ours projects.
+                                    Our projects.
                                 </div>
                             </div>
                         </div>
@@ -118,7 +118,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Cost of servises.
+                                    Cost of services.
                                 </div>
                             </div>
                         </div>
