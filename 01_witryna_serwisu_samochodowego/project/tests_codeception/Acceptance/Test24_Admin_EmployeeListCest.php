f7e6ffdece0a9c3c91ba8203f4df1de6a9a31f75
Jakub Owczarek <jakub.owczarke@gmail.com>
2023-01-15 12:26:38 +0100

    Add Test24 to see if admin can see employee on list

---
+++ 01_witryna_serwisu_samochodowego/project/tests_codeception/Acceptance/Test24_Admin_EmployeeListCest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace TestsCodeception\Acceptance;
+
+use TestsCodeception\Support\AcceptanceTester;
+
+class Test24_Admin_EmployeeListCest
+{
+    public function employeeListTest(AcceptanceTester $I): void
+    {
+        $I->wantTo('See employee on list');
+
+        $I->amOnPage('/login');
+        $I->seeCurrentUrlEquals('/login');
+        $I->fillField('email', 'admin@gmail.com');
+        $I->fillField('password', 'secret');
+        $I->click('Log in');
+        $I->seeCurrentUrlEquals('/dashboard');
+
+        $id = 420;
+        $name = 'Employee of the month';
+        $email = 'employee_of_the_month@gmail.com';
+        $password = 'qwerty123';
+        $type = 2;
+        $phone = 420420420;
+
+        $userID = $I->haveInDatabase('users', ['id' => $id, 'name' => $name,
+            'email' => $email, 'password' => $password, 'type' => $type, 'phone' => $phone]);
+
+        $I->amOnPage('/employees');
+        $I->seeCurrentUrlEquals('/employees');
+
+        $I->see($id);
+        $I->see($name);
+        $I->see($email);
+        $I->see($phone);
+    }
+}
